bc4d186919dfd4ddb9157cf12b3ecb5d5c080e39
Catrin Sauer <c.sauer@intershop.de>
2023-09-06 13:10:40 +0200

    initial commit

---
+++ src/app/books/dashboard/dashboard.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+export class DashboardComponent {
+  books: Book[];
+
+  constructor() {
+    this.books = [
+      {
+        isbn: '123456789',
+        title: 'Angular 2',
+        description: 'Grundlagen',
+        rating: 5,
+        price: 42.9,
+      },
+      {
+        isbn: '987654321',
+        title: 'Angular 2',
+        description: 'Fortgeschrittene Programmierung',
+        rating: 4,
+        price: 39.5,
+      },
+    ];
+  }
+}
